500e1f005367f0f339aa254ee8437b581310a466
nik110996 <116741391+nik110996@users.noreply.github.com>
2023-06-08 21:16:28 +0400

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Builder;
 import lombok.Data;
-import ru.yandex.practicum.filmorate.userProperty.Friendship;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
